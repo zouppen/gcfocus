93423db6a28dc877c949b5504ab6572a6b2e0737
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-08-30 13:10:57 +0300

    Add CMake and option parsing boilerplate

---
+++ src/main.c
@@ -0,0 +1,103 @@
+// Main module for gcfocus
+// SPDX-License-Identifier:   GPL-3.0-or-later
+// Copyright (C) 2021 Joel Lehtonen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <err.h>
+#include <errno.h>
+#include <glib.h>
+
+static gchar *from_bool(gboolean x);
+
+static gchar *camera_dev = NULL;
+static gchar *log_file = NULL;
+static gboolean log_truncate = FALSE;
+static gdouble cal_a = NAN;
+static gdouble cal_b = NAN;
+static gdouble cal_c = NAN;
+static gboolean verbose = FALSE;
+
+static GOptionEntry entries[] =
+{
+	{ "device", 'd', 0, G_OPTION_ARG_FILENAME, &camera_dev, "Device path to V4L2 compliant camera. Required.", "PATH"},
+	{ "log", 'f', 0, G_OPTION_ARG_FILENAME, &log_file, "Octoprint serial log file to read. Required.", "PATH"},
+	{ "truncate", 't', 0, G_OPTION_ARG_NONE, &log_truncate, "Truncate input log periodically to save space. Default: no truncate", NULL},
+	{ "a", 'a', 0, G_OPTION_ARG_DOUBLE, &cal_a, "Calibration constant a. Required.", "NUMBER"},
+	{ "b", 'b', 0, G_OPTION_ARG_DOUBLE, &cal_b, "Calibration constant b. Required.", "NUMBER"},
+	{ "c", 'c', 0, G_OPTION_ARG_DOUBLE, &cal_c, "Calibration constant c. Required.", "NUMBER"},
+	{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Verbose output. Report conversions and camera operations.", NULL},
+	{ NULL }
+};
+
+int main(int argc, char **argv)
+{
+	g_autoptr(GError) error = NULL;
+	g_autoptr(GOptionContext) context = g_option_context_new(NULL);
+	g_option_context_add_main_entries(context, entries, NULL);
+	g_option_context_set_summary(context, "Focus 3D printer or CNC machine camera based on X-axis position.");
+	g_option_context_set_description(context,
+					 "See calibration.md for instructions how to find constants a, b, and c.\n"
+					 "");
+
+	if (!g_option_context_parse(context, &argc, &argv, &error))
+	{
+		errx(1, "Option parsing failed: %s", error->message);
+	}
+
+	// Options validation
+	if (argc != 1) {
+		errx(1, "This command only expects flags, not arguments. See %s --help", argv[0]);
+	}
+
+	if (isnan(cal_a) || isnan(cal_b) || isnan(cal_c)) {
+		errx(1, "All calibration constants must be given. See %s --help", argv[0]);
+	}
+
+	if (camera_dev == NULL) {
+		errx(1, "Camera device path must be given. See %s --help", argv[0]);
+	}
+
+	if (log_file == NULL) {
+		errx(1, "Octoprint log file location must be given. See %s --help", argv[0]);
+	}
+
+	// Report startup variables
+	if (verbose) {
+		printf("Camera device path: %s\n"
+		       "Octoprint log path: %s\n"
+		       "Truncating logs: %s\n"
+		       "Calibration formula: f = %f/(x%+f)%+f\n",
+		       camera_dev,
+		       log_file,
+		       from_bool(log_truncate),
+		       cal_a,
+		       cal_b,
+		       cal_c);
+	}
+	
+	printf("TODO\n");
+	
+	return 0;
+}
+
+// Returns just string constants, no dynamic strings are allocated.
+static gchar *from_bool(gboolean x)
+{
+	return x ? "on" : "off";
+}
